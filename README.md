1bdf26be1c9f4f0696f74f87f42ce3cccb8d0de6
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-20 18:52:59 +0300

    Completed assignment

--- README.md
+++ README.md
@@ -15,4 +15,62 @@ assign your output to the finalOutput variable
 
 
 # YOUR README DESCRIPTION STARTS HERE
+STUDENT GRADE GENERATOR:
+About the program:
+This is a simple javascript program that prompts the user to put the marks that the student has gotten
+and then prints out the grade of the student as the output.
+
+
+Instructions on how to go through with creating the program;
+
+To begin with fork the assignment and create a new respository on your github account
+then understand the program;
+this program is simply telling us to propmt the user to give us a grade whenever we place a student marks
+
+The process of creating the program:
+first,prompt the user by using the word "prompt"
+then declare the necessary variables,
+lastly use the if ,else conditions to navigate through the program.
+put the output you would want it to showcase then go live,enter the student mark ,click on get grade,then the output will appear.
+
+
+
+SPEED DETECTOR (TOY PROBLEM);
+About the program:
+This is a simple javascript program that prompts the user to input the speed of a car (in kilometres per hour)  and calculates the demerit points for the driver based on the speed.if the demerit points are more than 12 then the program will print "license Suspended".
+
+Instructions on how to navigate through the program
+first understand the program and think of a way you would use to takle the problem.
+the process of creating the program is:
+first prompt the user by using the prompt("")
+
+declare the variables :speedlimit and demerit points
+after that use if,else conditions to navigate through the code,put the output expected,go live,input the speed and then click on points.
+
+this program is written strictly using the JavaScript programming language;
+
+
+
+NET SALARY CALCULATOR (TOY PROBLEM);
+About the program:
+This is another program that prompts the user to input the basic salary and benefits in order for it to be able to Calculate the payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary. 
+
+Instructions  and process on how to create the program
+first understand the question you are asked ;
+Declare the variables needed for example the:
+The Tax threshhold(exempt),the KRA RATES,the nhif rates,the nhif threshhold(exempt),
+Prompt the user to enter the basic salary and the benefits of the individual
+use the if else condition to solve the problem in the program 
+the console.log to get the outputs of the calculated payee (i.e. Tax), NHIFDeductions, NSSFDeductions, gross salary, and net salary. put your expected output
+
+after all that go live and try it by entering the basic salary amount and the benefits amount then click on "get net salary" and the output will showcase it self there.
+
+after that git add.   , commit and push your assignment and then submit your github url of the concerning respository.
+YOU ARE DONE WITH YOUR FIRST CODE CHALLANGE.
+
+AUTHOR:
+This project was created by WINNIE JOMO(winniegithubrit@github.com)
+
+LICENSE:
+This project is licensed under 
 
--- script.js
+++ script.js
@@ -9,40 +9,56 @@ let inputGrade=parseInt(document.getElementById('input-grade').value)
 console.log(inputGrade)
 let finalOutput
 // write Code For the Code challenge
+let grade = parseInt(prompt("enter the marks of the student that should be between 0 to 100").value);
+ grade = 42;
+if(grade >79){
+    finalOutput = ("A");
+}
+else if(grade > 60,grade < 79){
+    finalOutput = ("B");
+}
+else if(grade<59,grade>49){
+    finalOutput = ("C");
+}
+else if(grade<49,grade>40){
+    finalOutput = ("D")
+}
+else{
+    finalOutput = ("E");
+}
 
-
-
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
-document.getElementById('marks-id').append(finalOutput)
+//finalOutput='assign your final output to this variable'
+document.getElementById('marks-id').append(finalOutput);
 
 })
+let speedBtn = document.getElementById('speed');
 speed.addEventListener('click', (e)=>{
     e.preventDefault()
     let inputSpeed=parseInt(document.getElementById('input-speed').value)
     console.log(inputSpeed)
     let finalOutput
 // write Code For the Code challenge
+let speed = parseInt(prompt("enter the speed of the car( in km/hr):"));
+let speedLimit = 70;
+let hrdemeritPoints = 5;
+if (speed<=speedLimit){
+    finalOutput = "OK";
+}
+else {
+    let demeritPoints =Math.floor((speed - speedLimit) / hrdemeritPoints);
+    if (demeritPoints >= 12){
+        finalOutput =  "license suspended";
+    }else{
+        finalOutput = `Points: ${demeritPoints}`;
+    }
+    
+}
+console.log(finalOutput);
 
 
+    
 
-
-
-
-
-
-
-
-
-
-
-finalOutput='assign your final output to this variable'
+//finalOutput='assign your final output to this variable'
     document.getElementById('speed-id').append(finalOutput)
 
 })
@@ -50,9 +66,60 @@ salary.addEventListener('click', (e)=>{
         e.preventDefault()
         let inputSalary=parseInt(document.getElementById('input-salary').value)
         console.log(inputSalary)
-let finalOutput
+
 // write Code For the Code challenge
 
+const taxExempt = 28800;
+const kraRate1 = 0.1;
+const kraRate2 = 0.15;
+const kraRate3 = 0.20;
+const kraRate4 = 0.25;
+
+const nhifRates = [150,300,400,500,600,750,850,900,950];
+const nhifExempt = 6000;
+let basicSalary = parseFloat(prompt("Enter your basic salary: "));
+let benefits = parseFloat(prompt("Enter your benefits"));
+let grossSalary = basicSalary + benefits;
+let payee = 5000;
+if (grossSalary > taxExempt){
+    if (grossSalary <= 36000){
+        payee = (grossSalary - taxExempt) * kraRate1;
+    }else if(grossSalary <= 72000){
+        payee = 720 * kraRate1 + (grossSalary - 36000 - 720) * kraRate2;
+    }else if(grossSalary <=108000){
+        payee = 720 * kraRate1 + 2160 *kraRate2 + (grossSalary - 72000 - 2160) * kraRate3;
+    }else {
+        payee = 720 * kraRate1 + 2160 * kraRate2 + 3600 * kraRate3 + (grossSalary - 108000 -3600) * kraRate4;
+    }
+}
+
+let nhifDeductions = 1000;
+if(grossSalary > nhifExempt){
+    for(let i=1; i<nhifRates.length; i++){
+        if (grossSalary <=(i+1)*nhifExempt){
+            nhifDeductions = nhifRates[i];
+            break;
+        }
+    }
+}else{
+    nhifDeductions = nhifRates;
+}
+let nssfRates = 0.56;
+const nssfDeductions = grossSalary * nssfRates;
+
+const netSalary = grossSalary - payee - nhifDeductions - nssfDeductions;
+ finalOutput = `grossSalary :${grossSalary}
+payee:${payee}
+nhifDeductions:${nhifDeductions}
+nssfDeductions:${nssfDeductions}
+netSalary:${netSalary}`
+
+
+let Output = finalOutput;
+console.log(Output);
+
+
+ 
 
 
 
@@ -63,7 +130,7 @@ let finalOutput
 
 
 
-finalOutput ='assign your final output to this variable'
+//finalOutput ='assign your final output to this variable'
 document.getElementById('salary-id').append(finalOutput)
         
 })
