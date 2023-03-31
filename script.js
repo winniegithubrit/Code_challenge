8a72f7664f3f4ae62bf9457fbcb2488051314ea2
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-31 11:38:16 +0300

    Completed assignment

--- script.js
+++ script.js
@@ -9,23 +9,33 @@ let inputGrade=parseInt(document.getElementById('input-grade').value)
 console.log(inputGrade)
 let finalOutput
 // write Code For the Code challenge
-let grade = parseInt(prompt("enter the marks of the student that should be between 0 to 100").value);
- grade = 42;
-if(grade >79){
-    finalOutput = ("A");
+let marks = prompt("Please enter the student's marks (between 0 and 100):");
+// Convert marks to a number
+marks = Number(marks);
+
+// Check if marks are within the valid range
+if (marks < 0 || marks > 100) {
+  console.log(finalOutput("Invalid marks entered. Please enter a value between 0 and 100."));
+} else {
+  // assigning grades according to the marks gotten
+  let grade;
+  if (marks > 79) {
+    finalOutput=('Grade is A');
+  } else if (marks >= 60 && marks <= 79) {
+    finalOutput =('Grade is B');
+  } else if (marks >= 50 && marks <= 59) {
+    finalOutput=('Grade is C');
+  } else if (marks >= 40 && marks <= 49) {
+     finalOutput=('Grade is D');
+  } else {
+    finalOutput=('Grade is E');
+  }
+    grade = finalOutput
+  // the FinalOutput  
+  console.log(`The student's grade is ${finalOutput}.`);
+  
 }
-else if(grade > 60,grade < 79){
-    finalOutput = ("B");
-}
-else if(grade<59,grade>49){
-    finalOutput = ("C");
-}
-else if(grade<49,grade>40){
-    finalOutput = ("D")
-}
-else{
-    finalOutput = ("E");
-}
+
 
 //finalOutput='assign your final output to this variable'
 document.getElementById('marks-id').append(finalOutput);
@@ -38,23 +48,21 @@ speed.addEventListener('click', (e)=>{
     console.log(inputSpeed)
     let finalOutput
 // write Code For the Code challenge
-let speed = parseInt(prompt("enter the speed of the car( in km/hr):"));
-let speedLimit = 70;
-let hrdemeritPoints = 5;
-if (speed<=speedLimit){
-    finalOutput = "OK";
-}
-else {
-    let demeritPoints =Math.floor((speed - speedLimit) / hrdemeritPoints);
-    if (demeritPoints >= 12){
-        finalOutput =  "license suspended";
-    }else{
-        finalOutput = `Points: ${demeritPoints}`;
-    }
-    
-}
-console.log(finalOutput);
+// prompting the user to enetr a value
+ let speed = parseInt(prompt("Enter the speed of the car: "));
+  //let demeritPoints = 0;
 
+if (speed <= 70) {
+ console.log(finalOutput=("Ok"));
+ } else {
+  let demeritPoints = Math.floor((speed - 70) / 5);
+  console.log(finalOutput=(`Points ${demeritPoints}`))
+ if (demeritPoints > 12) {
+  console.log(finalOutput=("License suspended"));
+ } else {
+     console.log(`Demerit points: ${demeritPoints}`);
+  }
+ }
 
     
 
@@ -68,15 +76,16 @@ salary.addEventListener('click', (e)=>{
         console.log(inputSalary)
 
 // write Code For the Code challenge
-
+//declaring the various kra rates as variables
 const taxExempt = 28800;
 const kraRate1 = 0.1;
 const kraRate2 = 0.15;
 const kraRate3 = 0.20;
 const kraRate4 = 0.25;
-
+//declaring the nhif rates also as variables
 const nhifRates = [150,300,400,500,600,750,850,900,950];
 const nhifExempt = 6000;
+//declaring the basic salary as variables and prompting the user to enter the basic salary and the benefits
 let basicSalary = parseFloat(prompt("Enter your basic salary: "));
 let benefits = parseFloat(prompt("Enter your benefits"));
 let grossSalary = basicSalary + benefits;
@@ -92,7 +101,7 @@ if (grossSalary > taxExempt){
         payee = 720 * kraRate1 + 2160 * kraRate2 + 3600 * kraRate3 + (grossSalary - 108000 -3600) * kraRate4;
     }
 }
-
+//declaring the nhif deductions 
 let nhifDeductions = 1000;
 if(grossSalary > nhifExempt){
     for(let i=1; i<nhifRates.length; i++){
@@ -106,7 +115,7 @@ if(grossSalary > nhifExempt){
 }
 let nssfRates = 0.56;
 const nssfDeductions = grossSalary * nssfRates;
-
+//this is where the final output will come .....it will display the finalOutput
 const netSalary = grossSalary - payee - nhifDeductions - nssfDeductions;
  finalOutput = `grossSalary :${grossSalary}
 payee:${payee}
